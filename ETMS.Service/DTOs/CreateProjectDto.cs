4d8da906c19949b90d031e92abad219cab898f47
PCTR50 <PCTR50@server7.com>
2025-08-05 18:49:35 +0530

    Add Default Boards CheckBox to New Project Sign Up User Verification UI Change Color only UI updates optimized by not calling the api Adding new task by showing input inside the Board when clicked and adding on blur

--- ETMS.Service/DTOs/CreateProjectDto.cs
+++ ETMS.Service/DTOs/CreateProjectDto.cs
@@ -2,12 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ETMS.Service.DTOs;
 
-public class CreateProjectDto 
+public class CreateProjectDto
 {
     [Required(AllowEmptyStrings = false, ErrorMessage = "Project Name is Required.")]
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public int CreatedByUserId{ get; set; }
+    public int CreatedByUserId { get; set; }
+
+    public bool IsAddDefaultBoards { get; set; } = false;
 }
--- ETMS.Service/Services/ProjectService.cs
+++ ETMS.Service/Services/ProjectService.cs
@@ -17,6 +17,8 @@ public class ProjectService(IUnitOfWork unitOfWork, IMapper mapper) : IProjectSe
 
     private IGenericRepository<UserProjectRole> UserProjectRoleRepo => _userProjectRoleRepo ??= unitOfWork.GetRepository<UserProjectRole>();
 
+    private readonly IGenericRepository<Board> _boardRepository = unitOfWork.GetRepository<Board>();
+
     //Get user's project list just requires to be authorized
     public async Task<IEnumerable<ProjectDto>> GetUserProjectsAsync(int userId)
     {
@@ -67,7 +69,74 @@ public class ProjectService(IUnitOfWork unitOfWork, IMapper mapper) : IProjectSe
                 UserId = projectDto.CreatedByUserId,
                 RoleId = (int)RoleEnum.Admin
             });
+        if (projectDto.IsAddDefaultBoards)
+        {
+
+            await _boardRepository.AddRangeAsync(GetDefaultBoards(projectDto.CreatedByUserId, addedProject.Id));
+        }
         await unitOfWork.SaveChangesAsync();
+
         return mapper.Map<ProjectDto>(addedProject);
+
+
+    }
+
+    public static IEnumerable<Board> GetDefaultBoards(int createdbyUserId, int projectId)
+    {
+        var now = DateTime.UtcNow;
+        return new List<Board>
+            {
+                new Board
+                {
+                    Name = "To Do",
+                    Description = "Tasks yet to be started",
+                    ColorCode = "#129dfd", // Blue
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    CreatedByUserId = createdbyUserId,
+                    ProjectId = projectId
+                },
+                new Board
+                {
+                    Name = "In Progress",
+                    Description = "Tasks being worked on",
+                    ColorCode = "#f5a623", // Orange-like
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    CreatedByUserId = createdbyUserId,
+                    ProjectId = projectId
+                },
+                new Board
+                {
+                    ProjectId = projectId,
+                    Name = "In Review",
+                    Description = "Tasks under review",
+                    ColorCode = "#a259ff", // Purple
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    CreatedByUserId = createdbyUserId
+                },
+                new Board
+                {
+                    ProjectId = projectId,
+                    Name = "Completed",
+                    Description = "Tasks that are done",
+                    ColorCode = "#00c48c", // Green
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    CreatedByUserId = createdbyUserId
+                },
+                new Board
+                {
+                    ProjectId = projectId,
+                    Name = "Backlog",
+                    Description = "Future or deferred tasks",
+                    ColorCode = "#eb5757", // Red
+                    CreatedAt = now,
+                    UpdatedAt = now,
+                    CreatedByUserId = createdbyUserId
+                }
+            };
+
     }
 }
